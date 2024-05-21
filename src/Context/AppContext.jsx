5ed2b29abf119175d62380a3528797a631b26797
Zain <zainqlandar@gmail.com>
2024-05-21 17:12:44 +0500

    get error

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -5,25 +5,11 @@ const AppContext = createContext();
 export const AppProvider = ({ children }) => {
   const [isupdated, setIsUpdated] = useState(0)
   const [todo, setTodo] = useState(null)
-  const [crudItems, setCrudItems] = useState(() => {
-    const localData = localStorage.getItem('crudItems')
-    return localData ? JSON.parse(localData) : [{id: 1, message: 'Hello World'}, {id: 2, message: 'Hello Cat'}]
-  })
 
-  const setItems = (item) => {
-    setCrudItems(prevItem => [...prevItem, item ])
-  }
 
-  const deleteItem = (id) => {
-    setCrudItems(prevItem => prevItem.filter(item => item.id !== id))
-  }
-
-  useEffect(() => {
-    localStorage.setItem('crudItems', JSON.stringify(crudItems))
-  }, [crudItems])
 
   return (
-    <AppContext.Provider value={{crudItems, setItems, deleteItem, isupdated, setIsUpdated, setTodo, todo}} >
+    <AppContext.Provider value={{ setItems, deleteItem, isupdated, setIsUpdated, setTodo, todo}} >
       {children}
     </AppContext.Provider>
   );
